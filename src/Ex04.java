28282b66a02bd87101189a60207388b49d488865
Ramon Bernardo <103331784+RamonBernardoeo@users.noreply.github.com>
2023-01-10 21:20:49 -0300

    Update and rename ex04.java to Ex04.java

--- src/Ex04.java
+++ src/Ex04.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 // Receba a temperatura em graus Celsius. Calcule e mostre a sua temperatura convertida em fahrenheit 
-public class ex04 {
+public class Ex04 {
     public static void main(String[] args) {
         int celsius,fahrenheit;
         System.out.println("Digite a temperatura em Celsius: ");
