8719c80b3c25745dacaa28b1081f728a176f1653
Ramon Bernardo <103331784+RamonBernardoeo@users.noreply.github.com>
2023-01-10 21:21:17 -0300

    Update and rename ex05.java to Ex05.java

--- src/Ex05.java
+++ src/Ex05.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 // Receba os coeficientes A, B e C de uma equação do 2º grau (AX²+BX+C=0). Calcule e mostre as raízes reais (considerar que a equação possue2 raízes).
 
-public class ex05 {
+public class Ex05 {
     public static void main(String[] args) {
         double a,b,c;
         double x1,x2;
